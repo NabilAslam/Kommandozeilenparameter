e2a9bb265a1fb4804f194290b5a3430056398908
Nabil Aslam <aslam@versacommerce.de>
2023-10-05 14:58:13 +0200

    first commit

---
+++ greeting.js
@@ -0,0 +1,19 @@
+// Demonstration of the process.argv property
+// Calling the process module
+const process = require("process");
+
+// Printing process.argv property value
+const args = process.argv;
+
+
+let name = args[2]
+let alter = args[3]
+let stadt = args[4]
+
+// The message
+console.log(`Hallo ${name} du bist ${alter} jahre alt und kommst aus ${stadt}`);
+
+//Showing the values and indexes. The first 2 means 0 and 1 are the paths. Everything afterwards are the arguments.
+args.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+});
